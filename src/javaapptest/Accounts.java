5fe29a4ac236d62b5f43fc9c2df8d97007707efd
Shanghai22 <tangposjessa@gmail.com>
2024-09-05 14:54:28 +0800

    accounts input

---
+++ src/javaapptest/Accounts.java
@@ -0,0 +1,21 @@
+
+package javaapptest;
+
+
+public class Accounts {
+     String firstname, lastname, email, username, password;
+        int ID;
+    
+    public void addInfo(String fname, String lname, String umail, String uname, String pass, int UID){
+        
+        this.firstname=fname;
+        this.lastname=lname;
+        this.email=umail;
+        this.username=uname;
+        this.password=pass;
+       
+    }
+    
+    public void
+    
+}
